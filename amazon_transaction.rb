1e909847b41a90921d6ab60db9cd052e311d72f3
Ceora Ford <ceoraallthetime@gmail.com>
2018-07-30 14:04:03 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,10 +1,9 @@
 # Code your cash register here!
 class Amazontransaction
-  def initialize (total)
+  def initialize (total, discount)
   @total = 0 
   @items = []
-  
-    
+  @discount = 20 
   end 
 end 
   
